5a8e003404e61c3580c87d9dd63e0ed1e8222dbf
IHaveAStrongPassword <alexmchen0@gmail.com>
2024-07-18 14:59:18 -0700

    prepare to merge format

--- SfmlASECII/Assets/init.bat
+++ SfmlASECII/Assets/init.bat
@@ -1,4 +1,7 @@
-mklink IBMCGA+_8x8.png "D:\RL\LibFrontier\Assets\font\IBMCGA+_8x8.png"mklink IBMCGA+_8x8.font "D:\RL\LibFrontier\Assets\font\IBMCGA+_8x8.font"
+mklink IBMCGA+_8x8.png  "D:\RL\LibFrontier\Assets\font\IBMCGA+_8x8.png"
 
-mklink RF_8x8.png "D:\RL\LibFrontier\Assets\font\RF_8x8.png"
-mklink RF_8x8.font "D:\RL\LibFrontier\Assets\font\RF_8x8.font"
+mklink IBMCGA+_8x8.font "D:\RL\LibFrontier\Assets\font\IBMCGA+_8x8.font"
+
+mklink RF_8x8.png 	  "D:\RL\LibFrontier\Assets\font\RF_8x8.png"
+
+mklink RF_8x8.font      "D:\RL\LibFrontier\Assets\font\RF_8x8.font"
